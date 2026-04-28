8b485d3f410dde7c6381afa34b45c8a6dbfb7d6f
github.com/ib-bsb-br/ib-bsb-br.github.io <marioseixascardosoneto@gmail.com>
2026-04-28 13:17:20 -0300

    Fix backup finalization flow and remove redundant tab lookup

--- src/system.h
+++ src/system.h
@@ -124,14 +124,20 @@ struct System {
         wxString focusfile = cfg->Read("lastopenfile", "");
         int selection = -1;
         loop(i, numfiles) {
-            wxString filename;
-            cfg->Read(wxString::Format("lastopenfile_%d", i), &filename);
-            if (!LoadDB(filename) && filename == focusfile) selection = i;
+            wxString remembered;
+            cfg->Read(wxString::Format("lastopenfile_%d", i), &remembered);
+            if (remembered.IsEmpty()) continue;
+
+            auto message = LoadDB(remembered);
+            if (message.IsEmpty() && remembered == focusfile) {
+                selection = frame->notebook->GetSelection();
+            }
         }
 
         if (!filename.IsEmpty()) {
             LoadDB(filename);
-        } else if (selection >= 0) {
+        } else if (selection >= 0 &&
+                   selection < static_cast<int>(frame->notebook->GetPageCount())) {
             frame->notebook->SetSelection(selection);
         }
 
@@ -148,13 +154,13 @@ struct System {
         auto language = trans ? trans->GetBestTranslation("ts") : wxString("");
 
         if (language.Len() == 5 &&
-            !LoadDB(frame->app->GetDocPath("examples/tutorial-" + language + ".cts"))[0]) {
+            LoadDB(frame->app->GetDocPath("examples/tutorial-" + language + ".cts")).IsEmpty()) {
             return;
         }
 
         language.Truncate(2);
         if (language.Len() == 2 &&
-            !LoadDB(frame->app->GetDocPath("examples/tutorial-" + language + ".cts"))[0]) {
+            LoadDB(frame->app->GetDocPath("examples/tutorial-" + language + ".cts")).IsEmpty()) {
             return;
         }
 
